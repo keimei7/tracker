b2ad33b583fde5362fc24f540ccfed628836fff3
keimei7 <shidashida7@icloud.com>
2026-03-25 22:36:43 +0900

    fix firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,15 @@
-
- import { initializeApp, getApps, getApp } from "firebase/app";
+"use client";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC2Ql6E9BtBpz6fHBVddbzXBMYdxGvTz_c",
-  authDomain: "tracker-1-dab9c.firebaseapp.com",
-  projectId: "tracker-1-dab9c",
-  storageBucket: "tracker-1-dab9c.firebasestorage.app",
-  messagingSenderId: "107411142587",
-  appId: "1:107411142587:web:60e431abd0ac65c6ed769e",
-  measurementId: "G-01FF111Y14"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
